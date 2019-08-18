9dfc403aea4146aaf5cef3e4cdc9b64e06f217b8
Elwood Gary <kenneth.gary@va.gov>
2019-08-18 12:51:44 -0400

    Added isTextPermitted checkbox to PhoneField modal

--- src/platform/user/profile/vet360/components/PhoneField/EditModal.jsx
+++ src/platform/user/profile/vet360/components/PhoneField/EditModal.jsx
@@ -2,30 +2,10 @@ import React from 'react';
 import { merge } from 'lodash';
 
 import ErrorableTextInput from '@department-of-veterans-affairs/formation-react/ErrorableTextInput';
+import ErrorableCheckbox from '@department-of-veterans-affairs/formation-react/ErrorableCheckbox';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 
 import Vet360EditModal from '../base/EditModal';
-
-class PhoneTextInput extends ErrorableTextInput {
-  // componentDidMount() {
-  //   const wrapper = document.createElement('div');
-  //   wrapper.className = 'usa-only-phone-wrapper';
-
-  //   const inputField = document.querySelector('input.usa-only-phone');
-
-  //   inputField.parentNode.insertBefore(wrapper, inputField);
-  //   wrapper.appendChild(inputField);
-
-  //   const prefixEl = document.createElement('span');
-  //   prefixEl.innerText = '+1';
-  //   prefixEl.className = 'usa-only-phone-prefix';
-  //   inputField.insertAdjacentElement('beforebegin', prefixEl);
-  // }
-
-  render() {
-    return <ErrorableTextInput {...this.props} />;
-  }
-}
 
 export default class PhoneEditModal extends React.Component {
   // @todo Add propTypes
@@ -35,6 +15,7 @@ export default class PhoneEditModal extends React.Component {
   };
 
   onChange = field => ({ value, dirty }) => {
+    
     const newFieldValue = {
       ...this.props.field.value,
       [field]: value,
@@ -42,6 +23,16 @@ export default class PhoneEditModal extends React.Component {
 
     this.props.onChange(newFieldValue, dirty);
   };
+
+  onCheckboxChange = field => (value) => {
+   
+    const newFieldValue = {
+      ...this.props.field.value,
+      [field]: value,
+    };
+    
+    this.props.onChange(newFieldValue, true);    
+  }
 
   getInitialFormValues = () => {
     let defaultFieldValue;
@@ -57,10 +48,22 @@ export default class PhoneEditModal extends React.Component {
         countryCode: '1',
         extension: '',
         inputPhoneNumber: '',
+        isTextable: false,
+        isTextPermitted: false,
       };
     }
 
     return defaultFieldValue;
+  };
+
+  renderCheckbox = () => {
+    return (this.props.field.value.isTextable) 
+      ? <ErrorableCheckbox
+          label={<span>Receive text messages (SMS) for VA health care appointment reminders.</span>}
+          field={{ value: this.props.field.value.isTextPermitted, dirty: false }}
+          checked={this.props.field.value.isTextPermitted}
+          onValueChange={this.onCheckboxChange('isTextPermitted')} /> 
+      : null;
   };
 
   renderForm = () => (
@@ -72,7 +75,7 @@ export default class PhoneEditModal extends React.Component {
         </p>
       </AlertBox>
 
-      <PhoneTextInput
+      <ErrorableTextInput
         additionalClass="usa-only-phone"
         label="Number"
         charMax={14}
@@ -88,6 +91,9 @@ export default class PhoneEditModal extends React.Component {
         field={{ value: this.props.field.value.extension, dirty: false }}
         onValueChange={this.onChange('extension')}
       />
+      
+      {this.renderCheckbox()}
+          
     </div>
   );
 
